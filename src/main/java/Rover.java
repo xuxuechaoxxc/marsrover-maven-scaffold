f1c9ed73ebbaf175244705cdcb52fb9b39b42b71
xuxuechaoxxc <1209939784@qq.com>
2019-12-28 16:22:48 +0800

    refactor

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,18 +1,63 @@
 public class Rover {
+    public static final String EAST = "E";
+    public static final String NORTH = "N";
+    public static final String WEST = "W";
+    public static final String SOUTH = "S";
     private String e;
     private Area area;
-
     public void land(Area area, int x, int y, String e) {
+        if (x > area.getX()) {
+            throw new IllegalArgumentException("x=" + x + " is out of area width " + area.getX());
+        }
+        if (y > area.getY()) {
+            throw new IllegalArgumentException("y=" + y + " is out of area height " + area.getY());
+        }
         this.e = e;
         area.setX(x);
         area.setY(y);
         this.area = area;
     }
-
     public String getPosition() {
-        return ""+ area.getX() + "" + area.getY() + "" + e;
+        return String.format("%d%d%s", area.getX(), area.getY(), e);
     }
 
     public void move() {
+        if (this.e.equals("E")){
+                area.setX(area.getX() + 1);
+            }
+        if (this.e.equals("W")) {
+            area.setX(area.getX() - 1);
+        }
+        if (this.e.equals("N")) {
+            area.setY(area.getY() + 1);
+        }
+        if (this.e.equals("S")) {
+            area.setY(area.getY() - 1);
+        }
+    }
+
+    public void turnLeft() {
+        if (e.equals(EAST)) {
+            e = NORTH;
+        } else if (e.equals(NORTH)) {
+            e = WEST;
+        } else if (e.equals(WEST)) {
+            e = SOUTH;
+        } else if (e.equals(SOUTH)) {
+            e = EAST;
+        }
+        return;
+    }
+    public void turnRight() {
+        if (e.equals(EAST)) {
+            e = SOUTH;
+        } else if (e.equals(SOUTH)) {
+            e = WEST;
+        } else if (e.equals(WEST)) {
+            e = NORTH;
+        } else if (e.equals(NORTH)) {
+            e = EAST;
+        }
+        return;
     }
 }
--- src/main/java/RoverCon.java
+++ src/main/java/RoverCon.java
@@ -1,5 +1,26 @@
 public class RoverCon {
     public String excute(String mission) {
-        return null;
+        Rover rover = new Rover();
+        String[] commands = mission.split(",");
+        int areaWith = Integer.parseInt(commands[0]);
+        int areaHeight = Integer.parseInt(commands[1]);
+        int x = Integer.parseInt(commands[2]);
+        int y = Integer.parseInt(commands[3]);
+        Area area = new Area(areaWith, areaHeight);
+        for (int i = 4; i < commands.length; i++) {
+            if (commands[i].equals("L")) {
+                rover.turnLeft();
+            }
+            if (commands[i].equals("R")) {
+                rover.turnRight();
+            }
+            if (!commands[i].equals("L") && !commands[i].equals("R") && !commands[i].equals("M")) {
+                rover.land(area, x, y, commands[i]);
+            }
+            if (commands[i].equals("M")) {
+                rover.move();
+            }
+        }
+            return rover.getPosition();
+        }
     }
-}
